4e126337bfea6f04fbb38293057f730cd9aa0b46
ac2522 <52031988+ac2522@users.noreply.github.com>
2022-09-02 12:34:59 +0100

    Update conversion.py

--- utils/conversion.py
+++ utils/conversion.py
@@ -6,7 +6,7 @@ from rdkit import Chem
 RMV_SPACE = re.compile(r'\s+')
 
 
-# Removes whitespace and new line
+# Removes whitespace and new lines
 def un_tokenize(line):
     return re.sub(RMV_SPACE, "", line)
 
@@ -14,18 +14,19 @@ def un_tokenize(line):
 # Convert SMILES representation to InChI representation
 # Excessive recursion, broken into 4 cases:
 # Text, compounds, reactions, molecules
+# Simplistic function that will fail if a single molecule is invalid
 def smilesConversion(smiles, convert=lambda x : x):
 
     # list of molecules/reactions/compounds
     if type(smiles) is list:
-        return [smilesConversion(smile) for smile in smiles]
+        return [smilesConversion(smile, convert) for smile in smiles]
 
     elif "\n" in smiles:
         # txt file
         new_file = ""
 
         for smile in smiles.split("\n"):
-            new_file = new_file + smilesConversion(smile) + "\n"
+            new_file = new_file + smilesConversion(smile, convert) + "\n"
 
         return new_file[:-1]
 
@@ -34,7 +35,7 @@ def smilesConversion(smiles, convert=lambda x : x):
         compound = ""
 
         for smile in smiles.split("."):
-            compound = compound + smilesConversion(smile) + " . "
+            compound = compound + smilesConversion(smile, convert) + " . "
 
         return compound[:-3]
 
@@ -43,9 +44,9 @@ def smilesConversion(smiles, convert=lambda x : x):
         reaction = ""
 
         for smile in smiles.split("."):
-            reaction = reaction + smilesConversion(smile) + " . "
+            reaction = reaction + smilesConversion(smile, convert) + " . "
 
-        return inch[:-3]
+        return reaction[:-3]
 
     else:
         # Assume string
@@ -59,12 +60,17 @@ def smilesToInchI(smiles):
     return smilesConversion(smiles, convert)
 
 
-# SELFIES conversion
+# SMILES to SELFIES conversion
 def smilesToSelfies(smiles):
     return smilesConversion(smiles, sf.encoder)
 
 
+# SELFIES to SMILES conversion
+def selfiesToSmiles(selfies):
+    return smilesConversion(selfies, sf.decoder)
+
+
 # DeepSMILES conversion
-def smilesToSelfies(smiles):
+def smilesToDeepSmiles(smiles):
     converter = deepsmiles.Converter(rings=True, branches=True)
     return smilesConversion(smiles, converter)
